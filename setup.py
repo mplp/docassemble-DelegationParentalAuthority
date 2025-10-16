73cd319690ba4c0dddd115b7600c91bb042fd41c
Ramiro Lorca <110207826+RamiLorca@users.noreply.github.com>
2025-10-16 15:33:45 -0400

    Bumped version to 0.1.0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.DelegationParentalAuthority',
-      version='0.0.1',
+      version='0.1.0',
       description=('Delegation of Parental Authority'),
       long_description='# docassemble.DelegationOfParentalAuthorityE\r\n\r\nDelegation of parental authority\r\n\r\n## Author\r\n\r\nMichigan Legal Help\r\n\r\n',
       long_description_content_type='text/markdown',
